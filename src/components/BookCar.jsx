ea154968a312fea83dbd4c1d5885993ba2482f58
savarniknandinee <80510308+savarniknandinee@users.noreply.github.com>
2024-07-29 19:27:12 +0530

    Minor Changes

--- src/components/BookCar.jsx
+++ src/components/BookCar.jsx
@@ -174,7 +174,7 @@ function BookCar() {
     };
   
     try {
-      const response = await fetch('http://localhost:5000/send-email', {
+      const adminResponse = await fetch('http://localhost:5000/send-email', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
