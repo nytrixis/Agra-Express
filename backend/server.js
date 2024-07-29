a943e06c18696e9f3a97c5a922015502ffbd92f1
savarniknandinee <80510308+savarniknandinee@users.noreply.github.com>
2024-07-29 17:04:07 +0530

    Send Booking Mail to admin

--- backend/server.js
+++ backend/server.js
@@ -18,20 +18,58 @@ const transporter = nodemailer.createTransport({
 app.post('/send-email', (req, res) => {
   const { to, subject, formData } = req.body;
 
-  const mailOptions = {
+  const formatEmailContent = (data) => {
+    return `
+      Vehicle Type: ${data.vehicleType}
+      Vehicle Subtype: ${data.vehicleSubtype}
+      Vehicle Model: ${data.vehicleModel}
+      Pick-up Location: ${data.pickUp}
+      Drop-off Location: ${data.dropOff}
+      Pick-up Date and Time: ${data.pickTime}
+      Drop-off Date and Time: ${data.dropTime}
+
+      Personal Information:
+      Name: ${data.name} ${data.lastName}
+      Phone: ${data.phone}
+      Age: ${data.age}
+      Email: ${data.email}
+      Address: ${data.address}
+      City: ${data.city}
+      Zip Code: ${data.zipcode}
+    `;
+  };
+
+
+  const adminMailOptions = {
     from: process.env.EMAIL_USER,
     to: to,
     subject: subject,
-    text: JSON.stringify(formData, null, 2)
+    text: formatEmailContent(formData)
   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
+  const customerMailOptions = {
+    from: process.env.EMAIL_USER,
+    to: to,
+    subject: "Your Booking Confirmation E-mail",
+    text: `Dear ${formData.name},\n\nThank you for your booking. Here are your details:\n\n${formatEmailContent(formData)} \nOur team will get in touch with you soon.\n\n If you have any questions, please don't hesitate to reach out to us. \n\n\nBest regards,\nAgra Express Team.`
+  };
+
+  transporter.sendMail(adminMailOptions, (error, info) => {
     if (error) {
       console.log(error);
-      res.status(500).send('Error sending email');
+      res.status(500).send('Error sending email to admin');
     } else {
-      console.log('Email sent: ' + info.response);
+      console.log('Email sent to admin: ' + info.response);
       res.status(200).send('Email sent successfully');
+
+      transporter.sendMail(customerMailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.status(500).send('Error sending mail to customer');
+        } else {
+            console.log('Email sent to customer: ' +info.response);
+        }
+      });
     }
   });
 });
